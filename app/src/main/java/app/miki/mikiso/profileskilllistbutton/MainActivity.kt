754800dd1328e5e49c85c5b6ab9283ba541ca5dc
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-05-11 00:50:06 +0900

    TextView Button

---
+++ app/src/main/java/app/miki/mikiso/profileskilllistbutton/MainActivity.kt
@@ -0,0 +1,23 @@
+package app.miki.mikiso.profileskilllistbutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun click(view: View){
+
+        //ここに付け足したい機能を付ける
+
+
+    }
+
+
+
+}
